e229379ef3db773198f4ce490fa61d2c620cb577
Marc <marc_szymendera@live.de>
2024-04-19 11:22:35 +0200

    Navbar springt, die separaten Components sind jetzt angelegt. Alles wird jetzt je nach Roting über "router-outlet" eingespielt.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import {Component, HostListener} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
+  }
+
+  public navigateToSection(section: string) {
+    window.location.hash = '';
+    window.location.hash = section;
+  }
+
+
+}
