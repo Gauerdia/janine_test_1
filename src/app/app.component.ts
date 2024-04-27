b576acb2b1c866b87483c9dd118d837fd05ec2af
Marc <marc_szymendera@live.de>
2024-04-27 15:33:48 +0200

    Update für Responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,18 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 export class AppComponent {
   title = 'janine_test_1';
 
+
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
     let element = document.querySelector('.navbar') as HTMLElement;
-    if (window.pageYOffset > element.clientHeight) {
-      element.classList.add('navbar-inverse');
-    } else {
-      element.classList.remove('navbar-inverse');
-    }
+    element.classList.add('navbar-inverse-scrolled');
+
+    // Optional: Forced repaint for potential browser issues
+    window.requestAnimationFrame(() => {
+      element.classList.remove('navbar-inverse-scrolled');
+      element.classList.add('navbar-inverse-scrolled');
+    });
   }
-  
+
 
 }
